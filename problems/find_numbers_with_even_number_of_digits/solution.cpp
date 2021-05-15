99a4639a83167eba0408ffbba8647c592f3349dd
jeffreyjose07 <jeffreyjose.k@gmail.com>
2021-05-15 14:27:32 +0000

    Sync LeetCode submission - Find Numbers with Even Number of Digits (cpp)

--- problems/find_numbers_with_even_number_of_digits/solution.cpp
+++ problems/find_numbers_with_even_number_of_digits/solution.cpp
@@ -1,12 +1,37 @@
+namespace {
+    
+    constexpr bool even(int i)
+    {
+        unsigned digits = 0;
+        while(i)
+        {
+            ++digits;
+            i /= 10;
+        }
+        return (digits & 1u) == 0u;
+    }
+    
+    bool g_is_even[100001];
+    
+    static const auto initer=[]()
+    {
+        for(unsigned i = 0; i <= 100000; ++i){
+            g_is_even[i] = even(i);
+        }
+        return nullptr;
+    }();
+    
+}
+
 class Solution {
 public:
-    int findNumbers(vector<int>& nums) {
-        int count = 0;
-        for(int i=0;i<nums.size();++i){
-            if(int(log10(nums[i]) + 1)%2==0){
-                count++;
-            }
+    static int findNumbers(const vector<int>& nums) {
+        unsigned count = 0;
+        for(int i : nums)
+        {
+            count += i <= 100000 ? g_is_even[i] : even(i);
         }
         return count;
+        
     }
 };
