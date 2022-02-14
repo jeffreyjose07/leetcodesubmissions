2dfcf3c74331bec535d5f0abb4d56f3c480eb44a
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-02-14 05:36:18 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- problems/maximum_subarray/solution.cpp
+++ problems/maximum_subarray/solution.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int runSum = nums[0];
-        int maxSum = nums[0];
-        for(int i=1;i<nums.size();i++){
-            if (runSum < 0)
-            {
-                runSum = 0;
+        int max = INT_MIN;
+        int current_sum = 0;
+        for(int i = 0; i<nums.size();i++){
+            current_sum+=nums[i];
+            if(current_sum>max){
+                max = current_sum;
             }
-            
-            runSum = runSum + nums[i];
-            
-            if (runSum > maxSum)
-            {
-                maxSum = runSum;
+            if(current_sum<0){
+                current_sum = 0;
             }
         }
-        return maxSum;
+        return max;
     }
 };
