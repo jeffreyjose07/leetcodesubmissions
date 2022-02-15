f36a6c9f98401b3257b3f849d4062e9f0cf41741
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-02-15 10:14:41 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- problems/first_bad_version/solution.cpp
+++ problems/first_bad_version/solution.cpp
@@ -4,18 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        int mid = 0;
-        while(start<end){
-            mid = start + (end - start) / 2;
+        int l = 1,r = n,mid,sol;
+        while(l<=r){
+            mid = l+(r-l)/2;
             if(isBadVersion(mid)){
-                end = mid;
+                sol = mid;
+                r =mid-1; 
+            }else{
+                l = mid+1;
             }
-            else{
-                start = mid+1;    
-            }   
         }
-        return start;
+        return sol;
     }
 };
