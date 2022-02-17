8572ee8ec707cc8047375c4cad1bf578e337b44b
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-02-17 16:22:54 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

--- problems/move_zeroes/solution.cpp
+++ problems/move_zeroes/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
+        int pointer = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[count++]=nums[i];
-            }
+            if(nums[i]!=0)
+                nums[pointer++]=nums[i];
         }
-        for(int i=count;i<nums.size();i++){
-            nums[i]=0;
+        for(int i = pointer; i<nums.size(); i++){
+            nums[pointer++]=0;
         }
     }
 };
