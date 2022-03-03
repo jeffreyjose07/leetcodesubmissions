4079eabd579273ef889aeb063c6b9c913a892ec1
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-03-03 13:57:39 +0000

    Sync LeetCode submission - Counting Bits (cpp)

---
+++ problems/counting_bits/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>dp(n+1);
+        int offset = 1;
+        dp[0]=0;
+        for(int i = 1;i<n+1;i++){
+            if(offset*2==i){
+                offset = i;
+            }
+            dp[i]=1+dp[i-offset];
+        }
+        return dp;
+    }
+};
