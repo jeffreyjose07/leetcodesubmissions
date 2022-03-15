ff2f9b5d083447ddfd5bffec2a46ac2ec6500a54
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-03-15 10:33:50 +0000

    Sync LeetCode submission - Flood Fill (cpp)

--- problems/flood_fill/solution.cpp
+++ problems/flood_fill/solution.cpp
@@ -4,17 +4,17 @@ public:
         if(image[sr][sc]==newColor){
             return image;
         }
-        fill(image,sr,sc,image[sr][sc],newColor);
+        floodFillWithColor(image,sr,sc,newColor,image[sr][sc]);
         return image;
     }
-    void fill(vector<vector<int>>& image, int sr,int sc,int color,int newColor){
-        if(sr<0 || sc< 0 || sr==image.size() || sc == image[0].size() || image[sr][sc]!=color){
+    void floodFillWithColor(vector<vector<int>>& image, int sr, int sc, int newColor, int initialColor){
+        if(sr<0 || sr==image.size() || sc<0 || sc==image[0].size() || image[sr][sc]!=initialColor){
             return;
         }
         image[sr][sc]=newColor;
-        fill(image,sr-1,sc,color,newColor);
-        fill(image,sr,sc-1,color,newColor);
-        fill(image,sr,sc+1,color,newColor);
-        fill(image,sr+1,sc,color,newColor);
+        floodFillWithColor( image, sr-1,  sc,  newColor, initialColor);
+        floodFillWithColor( image, sr+1,  sc,  newColor, initialColor);
+        floodFillWithColor( image, sr,  sc-1,  newColor, initialColor);
+        floodFillWithColor( image, sr,  sc+1,  newColor, initialColor);
     }
 };
