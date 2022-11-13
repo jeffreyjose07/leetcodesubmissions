3834c1bee12955e7e1e2cb56f6933324458a7153
jeffreyjose07 <jeffreyjose.k@gmail.com>
2022-11-13 06:11:27 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

--- problems/contains_duplicate/solution.cpp
+++ problems/contains_duplicate/solution.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>hset;
-        for(auto it:nums){
-            if(hset.find(it)!=hset.end()){
+        unordered_map<int,int> hmap;
+        for(int i=0;i<nums.size();i++){
+            if(hmap.find(nums.at(i))==hmap.end()){
+               hmap[nums.at(i)]=1; 
+            }else{
+               hmap[nums.at(i)]++;
+            }
+        }
+        for(auto it=hmap.begin();it!=hmap.end();it++){
+            if(it->second>1){
                 return true;
             }
-            hset.insert(it);
         }
         return false;
     }
